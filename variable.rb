e704135dff3979a5c1adcf28b693b3f04a8b82f9
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-10-07 14:54:34 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,5 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 def variable (greeting)
-  greeting = "Hello World"
+  greeting = get_variable_from_file
 end
