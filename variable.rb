eb2965ef030771e5d84a300776c9d98e94af6831
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-10-07 14:47:34 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,6 +1,6 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-def local_variable( greeting) 
+def local_variable
   greeting = "Hello World"
   puts greeting
 end
